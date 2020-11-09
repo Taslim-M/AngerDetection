967cb2fa21ee86b0bb6f88441f4040fb352d1089
Taslim-M <taslimmahbub@hotmail.com>
2020-11-09 20:46:54 +0400

    Foundation for views

--- IoT/app.js
+++ IoT/app.js
@@ -15,5 +15,11 @@ app.use(express.static(__dirname+ "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+//Routes
+
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
 //start server
 app.listen(process.env.PORT || 3000, function () { console.log("Server Started"); });
