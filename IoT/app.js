8e3e56f2a515ec9303a9e190128be5fa1cf342dd
Taslim-M <taslimmahbub@hotmail.com>
2020-11-09 20:26:09 +0400

    make data

---
+++ IoT/app.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require('mongoose'),
+    User = require("./models/users"),
+    Device = require("./models/devices"),
+    Incident = require("./models/incidents"),
+    seedDb = require("./seeds");
+
+mongoose.connect('mongodb://localhost:27017/iot_anger', { useNewUrlParser: true, useUnifiedTopology: true });
+
+seedDb();
+
+app.use(express.static(__dirname+ "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+//start server
+app.listen(process.env.PORT || 3000, function () { console.log("Server Started"); });
