fb75ab816aeda00d3b1dedb0dc87e6ce774afb20
Taslim-M <taslimmahbub@hotmail.com>
2020-11-11 01:33:30 +0400

    Added New User Form and Feedback

--- IoT/app.js
+++ IoT/app.js
@@ -22,7 +22,40 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
     res.render("index");
 });
+app.get("/add_user", function (req, res) {
+    res.render("add_user");
+});
 
+app.post("/add_user", function (req, res) {
+    var user_id = req.body.user_id;
+    var name = req.body.name;
+    var mobile = req.body.mobile;
+    var email = req.body.email;
+    var guardian_str = req.body.guardian.split(',');
+    var guardian = [];
+    for(num of guardian_str){
+        guardian.push(Number(num));
+    }
+    console.log(guardian);
+    var user_to_add = {
+        "id": user_id,
+        "name":name,
+        "mobile": mobile,
+        "email": email,
+        "guardian" : guardian
+    }
+    console.log(user_to_add);
+    User.create(user_to_add, function(err, new_user){
+        if(err){
+            console.log(err);
+            res.render("add_user",{msg: "Sorry there was some error, try again"});
+        }else{
+            
+            res.render("add_user", {msg:"SUCCESS! Thank you for registering "+ new_user.name + ". Your ID is " + new_user.id});
+        }
+    });
+   
+});
 app.get("/incidents", async function (req, res) {
     var userID = req.query.userid;
     const users_found = await User.find({ 'guardian': userID });
