c57eeb9b19b58c831db3e1c0cf373fe4bcb01592
Azadan <azadanhb99@gmail.com>
2020-11-12 23:34:50 +0400

    Added Essentia trial code

--- IoT/Tf JS Microphone Trial/index.js
+++ IoT/Tf JS Microphone Trial/index.js
@@ -1,20 +1,25 @@
 async function readAudio() {
     const mic = await tf.data.microphone({
         fftSize: 1024,
-        columnTruncateLength: 232,
+        columnTruncateLength: 1500,
         numFramesPerSpectrogram: 43,
         sampleRateHz: 48000,
         includeSpectrogram: true,
         includeWaveform: true
     });
-    const audioData = await mic.capture();
-    const spectrogramTensor = audioData.spectrogram;
-    spectrogramTensor.print();
-    console.log("shape", spectrogramTensor.shape)
-    const waveformTensor = audioData.waveform;
-    waveformTensor.print();
-    console.log("shape", spectrogramTensor.shape)
-    mic.stop();
+    var x1 = Date.now();
+    
+    setTimeout(async () => {
+        const audioData = await mic.capture();
+        mic.stop();
+        console.log(Date.now()-x1);
+        const spectrogramTensor = audioData.spectrogram;
+        spectrogramTensor.print();
+        console.log("shape", spectrogramTensor.shape)
+        const waveformTensor = audioData.waveform;
+        waveformTensor.print();
+        console.log("shape", waveformTensor.shape)
+    }, 1500);
 }
 
 readAudio();
