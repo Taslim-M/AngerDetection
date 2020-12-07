ebdbe5629b891da98653d8c60baadbed82465ce7
Taslim-M <taslimmahbub@hotmail.com>
2020-12-07 21:33:35 +0400

    Added User List and Phone feature and Livestream

--- Web_Server/app.js
+++ Web_Server/app.js
@@ -85,7 +85,7 @@ app.post("/add_device", function (req, res) {
 app.get("/incidents", async function (req, res) {
     var userID = req.query.userid;
     const users_found = await User.find({ 'guardian': userID });
-
+   
     device_list = [];
     for (user of users_found) {
         const device = await Device.findOne({ "user_id": user.id });
@@ -96,12 +96,11 @@ app.get("/incidents", async function (req, res) {
     incidents_list = [];
     for (device of device_list) {
         const incidents_now = await Incident.find({ "device_id": device.device_id });
-        console.log(incidents_now);
+        // console.log(incidents_now);
         incidents_list.push(...incidents_now);
     }
-    console.log(incidents_list);
-    // res.send(incidents_list);
-    res.render("incidents", { incidents: incidents_list })
+    // console.log(incidents_list);
+    res.render("incidents", { incidents: incidents_list, users: users_found, devices:device_list  })
 
 });
 
@@ -152,4 +151,4 @@ app.post("/flutter_disable_notifs", function (req, res) {
 });
 
 //start server
-app.listen(process.env.PORT || 3000, function () { console.log("Server Started"); });
+app.listen(process.env.PORT || 3001, function () { console.log("Server Started"); });
